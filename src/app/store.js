750e1ee2302971900b0783d84bca343820d62b46
Vladislav <86360076+Zireael112@users.noreply.github.com>
2023-08-30 09:57:22 +0300

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import photoReducer from '../features/photo/photoSlice'
-
-export const store = configureStore({
-  reducer: {
-    photo: photoReducer,
-  }
-});
